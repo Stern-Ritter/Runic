3ac15ce33c6cc46d75d8826f7c2de990c7684d69
Stern-Ritter <fadeew.a@mail.ru>
2022-03-26 14:54:29 +0300

    Import create user form actions to index actions

--- src/services/actions/index.ts
+++ src/services/actions/index.ts
@@ -1,19 +1,21 @@
-export { 
-    START_ACTIVITY,
-    PAUSE_ACTIVITY,
-    RESUME_ACTIVITY,
-    ADD_COORDINATE,
-    SET_INDICATORS 
-} from "./map";
+export { START_ACTIVITY, PAUSE_ACTIVITY, RESUME_ACTIVITY, ADD_COORDINATE, SET_INDICATORS } from "./map";
 
 export {
-    GET_ACTIVITIES,
-    GET_ACTIVITIES_SUCCESS,
-    GET_ACTIVITIES_FAILED,
-    SET_ACTIVITIES_FILTERS,
-    CREATE_ACTIVITY,
-    CREATE_ACTIVITY_SUCCESS,
-    CREATE_ACTIVITY_FAILED,
-    getActivities,
-    createActivity
-} from './activities';
+  GET_ACTIVITIES,
+  GET_ACTIVITIES_SUCCESS,
+  GET_ACTIVITIES_FAILED,
+  SET_ACTIVITIES_FILTERS,
+  CREATE_ACTIVITY,
+  CREATE_ACTIVITY_SUCCESS,
+  CREATE_ACTIVITY_FAILED,
+  getActivities,
+  createActivity,
+} from "./activities";
+
+export {
+  CREATE_USER_FORM_SET_VALUE,
+  CREATE_USER_FORM_CLEAR_STATE,
+  CREATE_USER_FORM_SET_ERROR_MESSAGE,
+  setCreateUserFormValue,
+  setCreateUserFormErrorMessage,
+} from "./create-user-form";
