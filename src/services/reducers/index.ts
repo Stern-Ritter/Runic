fbc5b9143c448d231dc3439623db7b4f8a99039c
Stern-Ritter <fadeew.a@mail.ru>
2022-03-26 15:27:17 +0300

    Edit root reducer

--- src/services/reducers/index.ts
+++ src/services/reducers/index.ts
@@ -1,6 +1,12 @@
 import { combineReducers } from 'redux';
-import activityReducer from './map';
+import activitiesReducer from './activities';
+import mapReducer from './map';
+import authenticationReducer from './authentication-from';
+import createUserReducer from './create-user-form';
 
 export const rootReducer = combineReducers({
-    activity: activityReducer
+    createUserForm: createUserReducer,
+    authenticationForm: authenticationReducer,
+    map: mapReducer,
+    activities: activitiesReducer,
 });
