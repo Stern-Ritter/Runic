a9157ce6db523458583d6d9974909af4780de268
Stern-Ritter <fadeew.a@mail.ru>
2022-03-29 16:20:45 +0300

    Add LoggedInNvigation and LoggedOutNavigation to App component

--- App.tsx
+++ App.tsx
@@ -5,14 +5,18 @@ import AppLoading from "expo-app-loading";
 import { FontAwesome5 } from "@expo/vector-icons";
 import * as Font from "expo-font";
 import { Asset } from "expo-asset";
+import { useAuthState } from "react-firebase-hooks/auth";
 import LoggedInNavigation from "./src/navigators/logged-in-nav";
 import LoggedOutNavigation from "./src/navigators/logged-out-nav";
 import { store } from "./src/services/store/store";
+import { auth } from "./src/models/storage";
 
 // import * as Location from "expo-location";
 
 export default function App() {
   const [loading, setLoading] = useState(true);
+  const [user] = useAuthState(auth);
+
   // const [foregroundStatus, requestForegroundPermission] = Location.useForegroundPermissions();
   // const [backgroundStatus, requesBackgroundPermission] = Location.useBackgroundPermissions();
 
@@ -52,8 +56,7 @@ export default function App() {
   ) : (
     <Provider store={store}>
       <NavigationContainer>
-        <LoggedOutNavigation />
-        {/* <LoggedInNavigation /> */}
+        {user ? <LoggedInNavigation /> : <LoggedOutNavigation />}
       </NavigationContainer>
     </Provider>
   );
