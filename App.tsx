5b002fc2893723677a7c210f5e03cdbe464e3a37
Stern-Ritter <fadeew.a@mail.ru>
2022-04-05 05:26:49 +0300

    Fix typescript errors

--- App.tsx
+++ App.tsx
@@ -1,10 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import { Provider } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
-import AppLoading from "expo-app-loading";
-import { FontAwesome5 } from "@expo/vector-icons";
-import * as Font from "expo-font";
-import { Asset } from "expo-asset";
 import { useAuthState } from "react-firebase-hooks/auth";
 import LoggedInNavigation from "./src/navigators/logged-in-nav";
 import LoggedOutNavigation from "./src/navigators/logged-out-nav";
@@ -14,27 +10,9 @@ import "intl";
 import "intl/locale-data/jsonp/ru";
 
 export default function App() {
-  const [loading, setLoading] = useState(true);
   const [user] = useAuthState(auth);
 
-  const onLoading = async () => {
-    const fontsToLoad = [FontAwesome5.font];
-    const imagesToLoad = [require("./src/assets/images/rune.png")];
-    const fontsPromises = fontsToLoad.map((font) => Font.loadAsync(font));
-    const imagesPromises = imagesToLoad.map((image) => Asset.loadAsync(image));
-    await Promise.all([...fontsPromises, ...imagesPromises]);
-  };
-  const onFinish = () => {
-    setLoading(false);
-  };
-
-  return loading ? (
-    <AppLoading
-      startAsync={onLoading}
-      onFinish={onFinish}
-      onError={console.warn}
-    />
-  ) : (
+  return (
     <Provider store={store}>
       <NavigationContainer>
         {user ? <LoggedInNavigation /> : <LoggedOutNavigation />}
